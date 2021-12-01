7e36083aa3addaf67c95921502d455c11e596097
aljosa <aljosa.kancijanic@gmail.com>
2021-12-01 16:26:51 +0100

    Added comments to parts of code

--- DayOne/Part One/Submarines.py
+++ DayOne/Part One/Submarines.py
@@ -1,16 +1,22 @@
 import csv
 import numpy as np
 
+
+# Counter for the number of measurement that were higher than the one before
 larger_measurements = 0
+
 last_measure = 0
 
 file_in = open('input.txt', 'r')
 
-
+#We open the file and read each line as a number
 for row in file_in.read().split('\n'):
     if row.isdigit():
+        # Check for first measurement
         if last_measure == 0 :
             last_measure = float(row)
+
+        # Compare the current row in file with the previous measurement and increase counter if True
         if float(row) > last_measure:
             larger_measurements += 1
             print(float(row))
--- DayOne/Part Two/Submarines2.py
+++ DayOne/Part Two/Submarines2.py
@@ -8,6 +8,7 @@ sum_list = []
 file_in = open('input.txt', 'r')
 
 
+# Load values into a List
 for row in file_in.read().split('\n'):
     if row.isdigit():
         measurements_list.append(float(row))
@@ -18,7 +19,11 @@ print(len(measurements_list))
 prev_sum = 0
 current_sum = 0
 current_position = 0
+
+# Go over the imported list and create a new one with values that are equal to the sum of the current element and the next two that follow
 for item in measurements_list:
+
+    # If we can't form the requested sum ( we do not have 3 numbers ) we exit the loop
     if current_position+3 > len(measurements_list):
         break
     current_sum = item + measurements_list[current_position+1] + measurements_list[current_position+2]
@@ -27,6 +32,8 @@ for item in measurements_list:
 
 print(sum_list)
 
+
+# Same as part one , we just compare the values of sums
 for row in sum_list:
     if last_measure == 0:
         last_measure = float(row)
